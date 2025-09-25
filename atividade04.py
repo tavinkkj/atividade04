0278006a2e497b330a4a2a3ec9c0f0e0ff55421e
otavio <otavio.olivindo@aluno.ce.gov.br>
2025-09-25 14:38:18 -0300

    Add files via upload

---
+++ atividade04.py
@@ -0,0 +1,15 @@
+# Atividade 4 – Operações Matemáticas
+
+# Leia dois números e mostre:
+# A soma
+# A subtração
+# A multiplicação
+# A divisão
+numero01 = int(input("digite o primeiro número: "))
+numero02 = int(input("digite o segundo número: "))
+soma = numero01 + numero02
+subtracao = numero01 - numero02
+multiplicacao = numero01 * numero02
+divisao = numero01 / numero02
+print(f'sua soma é: {soma} sua subtracao é: {subtracao} sua multiplicacao é: {multiplicacao} sua divisao é: {divisao}')
+
